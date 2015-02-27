855d37f39ddf9c5d6d2532ba0443eb2e503da95c
Liam McCartney <liam.mccartney@gmail.com>
2015-02-27 15:25:09 -0500

    adds id and js_src attributes to grid items

--- items.js
+++ items.js
@@ -1,12 +1,15 @@
 var gridItems = [
-      { letter:"a" },
-      { letter:"b" },
-      { letter:"c" },
-      { letter:"d" },
-      { letter:"e" },
-      { letter:"f" },
-      { letter:"g" },
-      { letter:"h" }
+      { letter:"a",
+        id:"bd3e59e6-f6da-4738-a6de-2ea1e54421c6",
+        js_src:"js/scatter.js"
+      },
+      { letter:"b", id:"", js_src:"",  },
+      { letter:"c", id:"", js_src:"" },
+      { letter:"d", id:"", js_src:"" },
+      { letter:"e", id:"", js_src:"" },
+      { letter:"f", id:"", js_src:"" },
+      { letter:"g", id:"", js_src:"" },
+      { letter:"h", id:"", js_src:"" }
     ];
 
 var sideItems = [
