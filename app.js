d1eaf0a277a64e368a370d9c7232e3d5d3ff613f
Liam McCartney <liam.mccartney@gmail.com>
2015-03-12 16:34:40 -0400

    rms console.log

--- app.js
+++ app.js
@@ -73,7 +73,6 @@
     $scope.$watch(function(){return Bokeh.Collections("Plot").models.length}, function(){$scope.sizeElements()})
 
     $scope.$on('gridster-resized', function(event, newSizes){
-      console.log('resized!')
       $scope.sizeElements()
     });
   }])
