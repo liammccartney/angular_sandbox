28de22f30d99f31bbd004c49ef5316e5b891f493
Liam McCartney <liam.mccartney@gmail.com>
2015-04-08 17:16:03 -0400

    adds new plot to add from sidebar

--- items.js
+++ items.js
@@ -1,22 +1,26 @@
 var gridItems = [
       {
-        letter:"a",
+        title:"Line Plot",
         id:"1c4505be-4676-4d66-b3dc-166522105e51",
         js_src:"js/line.js",
         sizeX: 4,
         sizeY: 4
       },
       {
-        letter:"b",
-       id:"bd3e59e6-f6da-4738-a6de-2ea1e54421c6",
-       js_src:"js/scatter.js",
+        title:"Scatter Plot",
+        id:"bd3e59e6-f6da-4738-a6de-2ea1e54421c6",
+        js_src:"js/scatter.js",
         sizeX: 4,
         sizeY: 4
      }
     ];
 
 var sideItems = [
-  {name: "Visual 1"},
-  {name: "Visual 2"},
-  {name: "Visual 3"}
+     {
+      name: "Box Plot",
+      id: "8dc6e575-0ae6-4b64-bc54-a9ffe282b510",
+      js_src: "js/prop_graph.js",
+      sizeX: 8,
+      sizeY: 4
+     }
 ];
