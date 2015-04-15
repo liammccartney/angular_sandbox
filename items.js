82575ef2eba8ef36aeb09bfcebbb37970e20d6e7
Liam McCartney <liam.mccartney@gmail.com>
2015-04-15 15:49:15 -0400

    add all views to sideItems

--- items.js
+++ items.js
@@ -1,26 +1,32 @@
-var gridItems = [
-      {
-        title:"Line Plot",
-        id:"1c4505be-4676-4d66-b3dc-166522105e51",
-        js_src:"js/line.js",
-        sizeX: 4,
-        sizeY: 4
-      },
-      {
-        title:"Scatter Plot",
-        id:"bd3e59e6-f6da-4738-a6de-2ea1e54421c6",
-        js_src:"js/scatter.js",
-        sizeX: 4,
-        sizeY: 4
-     }
-    ];
+var gridItems = [];
 
 var sideItems = [
+     {
+       name:"Line Plot",
+       id:"1c4505be-4676-4d66-b3dc-166522105e51",
+       js_src:"js/line.js",
+       sizeX: 8,
+       sizeY: 8
+     },
+     {
+       name:"Scatter Plot",
+       id:"bd3e59e6-f6da-4738-a6de-2ea1e54421c6",
+       js_src:"js/scatter.js",
+       sizeX: 8,
+       sizeY: 8
+    },
      {
       name: "Box Plot",
       id: "8dc6e575-0ae6-4b64-bc54-a9ffe282b510",
       js_src: "js/prop_graph.js",
-      sizeX: 8,
-      sizeY: 4
+      sizeX: 16,
+      sizeY: 6
+     },
+     {
+      name: 'Another Graph',
+      id: "4221471a-a046-4b39-af5b-4e92c869bcfb",
+      js_src: "js/another_graph.js",
+      sizeX: 16,
+      sizeY: 6
      }
 ];
