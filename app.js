e28497f2b9876226f20ed26691980d203df0b8a2
Liam McCartney <liam.mccartney@gmail.com>
2015-04-15 15:48:41 -0400

    formats, adds protection against adding dupe views

--- app.js
+++ app.js
@@ -1,89 +1,88 @@
-(function() {
+Bokeh.set_log_level('warn')
+var app = angular.module('app', ['gridster', 'snap', 'ngDragDrop'])
 
-  var app = angular.module('app', ['gridster', 'snap', 'ngDragDrop'])
+app.config(function(snapRemoteProvider) {
+  snapRemoteProvider.globalOptions = {
+    disable: 'right',
+    touchToDrag: false
+  }
 
-  app.config(function(snapRemoteProvider) {
-    snapRemoteProvider.globalOptions = {
-      disable: 'right',
-      touchToDrag: false
+});
+
+app.controller('sideBarCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
+  $scope.name = 'sidebar'
+  $scope.items = sideItems
+
+  $scope.addItem = function(index){
+    if (gridItems.filter(function(v, i){return v === sideItems[index]}).length === 0) {
+      gridItems.push(sideItems[index])
+    };
+  }
+}])
+
+app.controller('dashCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
+
+  $scope.items = gridItems
+  $scope.bokehPlotCount = Bokeh.Collections("Plot").length
+  $scope.gridsterOpts = {
+    columns: 16,
+    margins: [10, 10],
+    pushing: false,
+    minColumns: 1,
+    minRows: 2,
+    defaultSizeX: 1,
+    defaultSizeY: 1,
+    minSizeX: 1,
+    maxSizeX: null,
+    minSizeY: 1,
+    maxSizeY: null,
+    resizable: {
+       enabled: false
+    },
+    draggable: {
+       enabled: false
     }
+  };
 
+  $scope.rearrangeGrid = function(){
+    if (this.gridsterOpts.draggable.enabled == true) {
+      this.gridsterOpts.draggable.enabled = false
+      this.gridsterOpts.pushing = false
+    }
+    else{
+      this.gridsterOpts.draggable.enabled = true
+      this.gridsterOpts.pushing = true
+    }
+  }
+
+  $scope.sizeElements = function(){
+    var plots = Bokeh.Collections("Plot")
+    var plotContexts = Bokeh.Collections("PlotContext")
+    plotContexts.each(function(plotContext){
+      var plotContextView = Bokeh.index[plotContext.id]
+      var plotsToSize = plots.filter(function(e){return e.attributes.doc === plotContext.attributes.doc})
+      plotsToSize.forEach(function(p){
+        p.set("plot_width", $(plotContextView.el).width() - 25 )
+        p.set("plot_height", $(plotContextView.el).height() -25 )
+      })
+    })
+
+    var dataTableElements = $(".bk-data-table")
+    dataTableElements.each(function(index, tableElement){
+      var modelId = findModelID($(tableElement).closest(".plotdiv")[0].id)
+      var dataTableView = findViewObject(tableElement, Bokeh.index[modelId])
+      dataTableView.model.attributes.width = dataTableView.$el.closest('.plotdiv').width() - 25
+      dataTableView.model.attributes.height = dataTableView.$el.closest('.plotdiv').height() - 40
+      dataTableView.render()
+    })
+  }
+
+  $scope.$watch(function() {return Bokeh.Collections("Plot").length}, function(newVal, oldVal, scope){
+    scope.sizeElements();
+  })
+
+  $scope.$on('gridster-resized', function(event){
+    $scope.sizeElements()
   });
 
-  app.controller('sideBarCtrl', ['$scope', function($scope){
-    $scope.name = 'sidebar'
-    $scope.items = sideItems
-
-    $scope.addItem = function(index){
-      if (gridItems.length < 16) {
-        gridItems.push(sideItems[index])
-      };
-    }
-  }])
-
-  app.controller('dashCtrl', ['$scope', function($scope){
-
-    $scope.items = gridItems
-
-    $scope.gridsterOpts = {
-      columns: 8,
-      margins: [10, 10],
-      pushing: false,
-      minColumns: 1, // the minimum columns the grid must have
-      minRows: 2, // the minimum height of the grid, in rows
-      defaultSizeX: 1, // the default width of a gridster item, if not specifed
-      defaultSizeY: 1, // the default height of a gridster item, if not specified
-      minSizeX: 1, // minimum column width of an item
-      maxSizeX: null, // maximum column width of an item
-      minSizeY: 1, // minumum row height of an item
-      maxSizeY: null, // maximum row height of an item
-      resizable: {
-         enabled: false
-      },
-      draggable: {
-         enabled: false
-      }
-    };
-
-    $scope.rearrangeGrid = function(){
-      if (this.gridsterOpts.draggable.enabled == true) {
-        this.gridsterOpts.draggable.enabled = false
-        this.gridsterOpts.pushing = false
-      }
-      else{
-        this.gridsterOpts.draggable.enabled = true
-        this.gridsterOpts.pushing = true
-      }
-    }
-
-    $scope.sizeElements = function(){
-      var plots = Bokeh.Collections("Plot")
-      var plotContexts = Bokeh.Collections("PlotContext")
-      plotContexts.each(function(plotContext){
-        var plotContextView = Bokeh.index[plotContext.id]
-        var plotsToSize = plots.filter(function(e){return e.attributes.doc === plotContext.attributes.doc})
-        plotsToSize.forEach(function(p){
-          p.set("plot_width", $(plotContextView.el).width() - 25 )
-          p.set("plot_height", $(plotContextView.el).height() -25 )
-        })
-      })
-
-      var dataTableElements = $(".bk-data-table")
-      dataTableElements.each(function(index, tableElement){
-        var modelId = findModelID($(tableElement).closest(".plotdiv")[0].id)
-        var dataTableView = findViewObject(tableElement, Bokeh.index[modelId])
-        dataTableView.model.attributes.width = dataTableView.$el.closest('.plotdiv').width() - 25
-        dataTableView.model.attributes.height = dataTableView.$el.closest('.plotdiv').height() - 40
-        dataTableView.render()
-        console.log('data table resized!')
-      })
-    }
-
-    $scope.$watch(function(){return Bokeh.Collections("Plot").models.length}, function(){$scope.sizeElements()})
-
-    $scope.$on('gridster-resized', function(event){
-      $scope.sizeElements()
-    });
-  }])
-
-})();
+}])
